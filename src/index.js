c0027e60a7b1a2d9133f4c3ebed22d852102c28a
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-25 16:30:16 +0100

    function onClick change into arrow function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square" 
-            onClick={function()
-                { alert('click');}}>
+            onClick={()=> alert('click')}>
                 {this.props.value}
             </button>
         );
